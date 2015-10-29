8c73ce8dcdde80b649382d0baddab297acffbb95
areynaldos <aaron_reynaldos@dell.com>
2015-10-29 11:55:22 -0600

    run-transformer.sh - Cosmetic change

--- dmw/run-transformer.sh
+++ dmw/run-transformer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
-echo "Unpacking dmw-transformer.zip"
+# echo "Unpacking dmw-transformer.zip"
 #rm -rf dmw-transformer
 #unzip dmw-transformer.zip
 export JAVA_OPTS="-Xms$1 -Xmx$1 -XX:+AggressiveOpts -XX:+AggressiveHeap -verbose:gc -XX:-PrintGCDetails -XX:-PrintGCTimeStamps -Dlicense.dir=. -Dlog4j.configuration=file:log4j2.xml -Ddmw-context=$2 -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:FlightRecorderOptions=defaultrecording=true,disk=true,dumponexit=true,maxsize=2G,settings=DMW.jfc"
