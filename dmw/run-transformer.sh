290622c37a5605532913ff6d01f969ff10ea6edc
areynaldos <aaron_reynaldos@dell.com>
2015-10-29 11:56:25 -0600

    run-transformer: Remove flight recorder

--- dmw/run-transformer.sh
+++ dmw/run-transformer.sh
@@ -2,7 +2,13 @@
 # echo "Unpacking dmw-transformer.zip"
 #rm -rf dmw-transformer
 #unzip dmw-transformer.zip
+
+# The following has the Flight Recorder on for debugging.
+#export JAVA_OPTS="-Xms$1 -Xmx$1 -XX:+AggressiveOpts -XX:+AggressiveHeap -verbose:gc -XX:-PrintGCDetails -XX:-PrintGCTimeStamps -Dlicense.dir=. -Dlog4j.configuration=file:log4j2.xml -Ddmw-context=$2 -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:FlightRecorderOptions=defaultrecording=true,disk=true,dumponexit=true,maxsize=2G,settings=DMW.jfc"
+
+# The following does not have Flight Recorder on.
 export JAVA_OPTS="-Xms$1 -Xmx$1 -XX:+AggressiveOpts -XX:+AggressiveHeap -verbose:gc -XX:-PrintGCDetails -XX:-PrintGCTimeStamps -Dlicense.dir=. -Dlog4j.configuration=file:log4j2.xml -Ddmw-context=$2 -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:FlightRecorderOptions=defaultrecording=true,disk=true,dumponexit=true,maxsize=2G,settings=DMW.jfc"
+
 export CLASSPATH_PREFIX=$(find lib -name '*.jar' -print | tr '\n' ':')
 echo "JAVA OPTS"
 echo $JAVA_OPTS
